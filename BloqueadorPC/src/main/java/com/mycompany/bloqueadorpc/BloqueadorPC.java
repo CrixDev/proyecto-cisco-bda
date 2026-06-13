73b63298e2007ef34be161ba9b5c91648c46e20c
CrixDev <devoracristian100@gmail.com>
2026-06-13 15:55:51 -0700

    setup del proyecto

---
+++ BloqueadorPC/src/main/java/com/mycompany/bloqueadorpc/BloqueadorPC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bloqueadorpc;
+
+/**
+ *
+ * @author Cristian Devora
+ */
+public class BloqueadorPC {
+
+    public static void main(String[] args) {
+        System.out.println("Programa 1 - Bloqueador de PC");
+    }
+}
